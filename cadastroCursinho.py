23711dd256eb20ef955ee28b6121decd63a9d2ac
JoaisBasilio <joais.silva@dce.ufpb.br>
2016-06-15 13:40:56 -0300

    Add files via upload

--- cadastroCursinho.py
+++ cadastroCursinho.py
@@ -1,15 +1,69 @@
-def dadosDoAluno(numeroCpf):
+def dadosDoAluno():
+    arquivo = open("alunos.txt","r") 
+    aluno = arquivo.readlines() 
+    arquivo.close()
+    numeroCpf = str(input(":...:: DIGITE O CPF DO ALUNO ::...:"))
+    while(numeroCpf == ""):
+        numeroCpf = input(":...:: DIGITE O CPF DO ALUNO ::...:")
+    achou = False
+    for i in range(2,len(aluno),11):
+        if(aluno[i] == numeroCpf+"\n"): 
+            print("")
+            print("")
+            print("\n:...:: NOME DO ALUNO ::...: \n"+aluno[i-2]) 
+            print("\n:...:: IDADE DO ALUNO ::...: \n"+aluno[i-1])
+            print("\n:...:: CPF DO ALUNO ::...: \n"+aluno[i])
+            print("\n:...:: O ALUNO É PORTADOR DE ALGUMA DEFICIÊNCIA? ::...: \n"+aluno[i+1])
+            print("\n:...:: ETINIA DO ALUNO ::...: \n"+aluno[i+2])
+            print("\n:...:: TIPO DE ESCOLA DO ALUNO ::...: \n"+aluno[i+3])
+            print("\n:...:: FILIAÇÃO - PAI ::...: \n"+aluno[i+4])
+            print("\n:...:: FILIAÇÃO - MÃE ::...: \n"+aluno[i+5])
+            print("\n:...:: CIDADE ::...: \n"+aluno[i+6])
+            print("\n:...:: ESTADO ::...: \n"+aluno[i+7])
+            print("\n:...:: E-MAIL ::...: \n"+aluno[i+8])
+            print("")
+            print("")
+            achou = True
+            break
+    if(achou == False):
+        print("")
+        print("")
+        print(":...:: CPF INEXISTENTE ::...: ")
+        print("")
+        print("")
+
+def exibeMenu():
+    print ("======================================================================")
+    print ("==========/------------------------------------------------\==========")
+    print ("=========/              1 = CADASTRAR NOVO ALUNO.           \=========")
+    print ("========|               2 = DADOS DO ALUNO.                  |========")
+    print ("========\               3 = FECHAR O PROGRMA.                /========")
+    print ("=========\--------------------------------------------------/=========")
+    print ("======================================================================")
+    opcaomenu = int(input(":...:::::::::::::::::::: O QUE DESEJA FAZER? ::::::::::::::::::::...: "))
+    return opcaomenu
+
+def cadastro():
+    print("")
+    print("")
+    nome = input(":...:: NOME DO ALUNO ::...: ")          
+    idade = input(":...:: IDADE DO ALUNO ::...: ")                
+    cpf = input(":...:: CPF DO ALUNO ::...: ")
+    deficiencia = input(":...:: O ALUNO É PORTADOR DE NECESSIDADES ESPECIAIS? ::...: ")
+    cor = input("...:: QUAL O TIPO DE ETINIA DO ALUNO? ::...: ")
+    escola = input(":...:: O ALUNO É DE ESCOLA PÚBLICA OU PARTICULAR? ::...: ")
+    pai = input(":...:: FILIAÇÃO - PAI ::...: ")
+    mae = input(":...:: FILIAÇÃO - MÃE ::...: ")
+    cidade = input(":...:: CIDADE ::...: ")
+    estado = input(":...:: ESTADO ::...: ")
+    email = input(":...:: E-MAIL ::...: ")
+    arquivo = open("alunos.txt","a")
+    arquivo.writelines([nome+"\n",idade+"\n",cpf+"\n", deficiencia+"\n", cor+"\n", escola+"\n", pai+"\n", mae+"\n", cidade+"\n", estado+"\n", email+"\n"])
+    arquivo.close()
+    print ("")
+    print (":...:: MUITO OBRIGADO, O ALUNO JÁ ESTA CADASTRADO ::...:")
+    print ("")
     
-    for i in range(len(alunos)):
-        
-        if(alunos[i][2] == numeroCpf):            
-            return(alunos[i])
-    else:
-        
-        return False
-
-alunos = []
-
 
 print ("======================================================================")
 print ("======================================================================")
@@ -24,13 +78,6 @@ print ("========|                                                    |========")
 print ("========|         CAMPUS IV - RIO TINTO E MAMANGUAPE         |========")
 print ("========\        OBRIGADO POR ACREDITAR NESSE PROJETO        /========")
 print ("=========\--------------------------------------------------/=========")
-print ("======================================================================")
-print ("==========/------------------------------------------------\==========")
-print ("=========/              1 = CADASTRAR NOVO ALUNO.           \=========")
-print ("========|               2 = DADOS DO ALUNO.                  |========")
-print ("========\               3 = FECHAR O PROGRMA.                /========")
-print ("=========\--------------------------------------------------/=========")
-print ("======================================================================")
 print ("==========/------------------------------------------------\==========")
 print ("=========/         EDUCAÇÃO NÃO TRANSFORMA O MUNDO.         \=========")
 print ("========|          EDUCAÇÃO MUDA AS PESSOAS.                 |========")
@@ -48,65 +95,11 @@ print ("")
 continuar = "sim"
 
 while(continuar=="sim"):
-        opcaomenu = int(input(":...:::::::::::::::::::: O QUE DESEJA FAZER? ::::::::::::::::::::...: "))
-
-
+        opcaomenu = exibeMenu()
         if (opcaomenu == 1):
-
-                print("")
-                print("")
-                nome = str(input(":...:: NOME DO ALUNO ::...: "))                
-                idade = input(":...:: IDADE DO ALUNO ::...: ")                
-                cpf = str(input(":...:: CPF DO ALUNO ::...: "))
-                deficiencia = str.lower(input(":...:: O ALUNO É PORTADOR DE NECESSIDADES ESPECIAIS? ::...: "))
-                cor = str(input("...:: QUAL O TIPO DE ETINIA DO ALUNO? ::...: "))
-                escola = str(input(":...:: O ALUNO É DE ESCOLA PÚBLICA OU PARTICULAR? ::...: "))
-                pai = str(input(":...:: FILIAÇÃO - PAI ::...: "))
-                mae = str(input(":...:: FILIAÇÃO - MÃE ::...: "))
-                cidade = str(input(":...:: CIDADE ::...: "))
-                estado = str(input(":...:: ESTADO ::...: "))
-                email = str(input(":...:: E-MAIL ::...: "))
-                alunos.append([nome,idade,cpf, deficiencia, cor, escola, pai, mae, cidade, estado, email])
-                print ("")
-                print (":...:: MUITO OBRIGADO, O ALUNO JÁ ESTA CADASTRADO ::...:")
-                print ("")
+            cadastro()    
         elif(opcaomenu == 2):
-
-            
-            localizar = str(input(":...:: DIGITE O CPF DO ALUNO ::...:"))
-                       
-            while(localizar == ""):
-                localizar = input(":...:: DIGITE O CPF DO ALUNO ::...:")
-
-            
-            aluno = dadosDoAluno(localizar)
-
-            
-            if(aluno != False):
-                
-                print("")
-                print("")
-                print("\n:...:: NOME DO ALUNO ::...: \n"+aluno[0])
-                print("\n:...:: IDADE DO ALUNO ::...: \n"+aluno[1])
-                print("\n:...:: CPF DO ALUNO ::...: \n"+aluno[2])
-                print("\n:...:: O ALUNO É PORTADOR DE ALGUMA DEFICIÊNCIA? ::...: \n"+aluno[3])
-                print("\n:...:: ETINIA DO ALUNO ::...: \n"+aluno[4])
-                print("\n:...:: TIPO DE ESCOLA DO ALUNO ::...: \n"+aluno[5])
-                print("\n:...:: FILIAÇÃO - PAI ::...: \n"+aluno[6])
-                print("\n:...:: FILIAÇÃO - MÃE ::...: \n"+aluno[7])
-                print("\n:...:: CIDADE ::...: \n"+aluno[8])
-                print("\n:...:: ESTADO ::...: \n"+aluno[9])
-                print("\n:...:: E-MAIL ::...: \n"+aluno[10])
-                print("")
-                print("")
-
-            
-            else:
-                print("")
-                print("")
-                print(":...:: CPF INEXISTENTE ::...: ")
-                print("")
-                print("")
+            dadosDoAluno() 
 
         elif(opcaomenu == 3):
             continuar = "nao"
